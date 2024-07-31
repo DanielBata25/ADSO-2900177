fd277ba8d8d3e96706bb244128e538d209fda324
DanielBata25 <142624559+DanielBata25@users.noreply.github.com>
2024-07-31 05:48:10 -0500

    Update fuction.js

--- Evidencias Andres/TABLA BINGO/js/fuction.js
+++ Evidencias Andres/TABLA BINGO/js/fuction.js
@@ -28,37 +28,37 @@ for(let iterador = 0; iterador<bingo.length; iterador++) {
 document.getElementById('bingo').innerHTML = pantalla;
 */
 
-let bingo=[];
-let interno =[];
+let bingo = [];
+let interno = [];
 let interacion1;
 let interacion2;
-let contador = 0;
+let contador = 1;
 let tabla;
-let cantidadPares = 0;
-let cantidadImpares = 0;
 let printResultado = ''
 
+
 // operacion del bingo
-for(interacion1 = 0; interacion1<5;interacion1++){
-    interno=[]; 
-    for(interacion2 = 0;interacion2<5;interacion2++ ){
-       contador = contador+1;
-       tabla = contador*3;
-       interno.push(tabla);
+for (interacion1 = 0; interacion1 < 5; interacion1++) {
+    interno = [];
+    for (interacion2 = 0; interacion2 < 5; interacion2++) {
+        contador = contador + 1;
+        tabla = contador * 3;
+        interno.push(tabla);
     }
     bingo.push(interno);
 }
 
 //boton para otros bingos 
-printResultado +='<div class="btn-group" role="group" aria-label="Basic example">'
-printResultado +='<button type="button" class="btn btn-primary" id="columna1">Fila 1</button>'
-printResultado +='<button type="button" class="btn btn-primary" id="columna2">Fila 2</button>'
-printResultado +='<button type="button" class="btn btn-primary" id="columna3">Fila 3</button>'
-printResultado +='<button type="button" class="btn btn-primary" id="columna4">Fila 4</button>'
-printResultado +='<button type="button" class="btn btn-primary" id="columna5">Fila 5</button>'
-printResultado +='<button type="button" class="btn btn-primary" id="X1">Una X1</button>'
-printResultado +='<button type="button" class="btn btn-primary" id="X3">Tres X3</button>'
-printResultado +='</div>'
+printResultado += '<div class="btn-group" role="group" aria-label="Basic example">'
+printResultado += '<button type="button" class="btn btn-primary" id="columna1">Fila 1</button>'
+printResultado += '<button type="button" class="btn btn-primary" id="columna2">Fila 2</button>'
+printResultado += '<button type="button" class="btn btn-primary" id="columna3">Fila 3</button>'
+printResultado += '<button type="button" class="btn btn-primary" id="columna4">Fila 4</button>'
+printResultado += '<button type="button" class="btn btn-primary" id="columna5">Fila 5</button>'
+printResultado += '<button type="button" class="btn btn-primary" id="xGrande">xGrand</button>'
+printResultado += '<button type="button" class="btn btn-primary" id="xPequeña">xPequeña</button>'
+printResultado += '<button type="button" class="btn btn-primary" id="x2Pequeña">x2Peuqeña</button>'
+printResultado += '</div>'
 
 
 
@@ -74,214 +74,300 @@ printResultado += ` <th >B</th>
 printResultado += '</tr>'
 printResultado += '</thead>'
 printResultado += ' <tbody>'
-for(interacion1 = 0; interacion1<5;interacion1++){
-         printResultado += '<tr>'
-         for(interacion2 = 0;interacion2<5;interacion2++ ){
-          
-            if(interacion2 == 0){
-          printResultado +='<td class="cambio1">'+bingo[interacion1][interacion2]+'</td>'    
-          }else if(interacion2 == 1){
-            printResultado +='<td class="cambio2">'+bingo[interacion1][interacion2]+'</td>'
-          }else if(interacion2 == 2){
-            printResultado +='<td class="cambio3">'+bingo[interacion1][interacion2]+'</td>'
-          }else if(interacion2 == 3){
-            printResultado +='<td class="cambio4">'+bingo[interacion1][interacion2]+'</td>'
-          }else if(interacion2 == 4){
-            printResultado +='<td class="cambio5">'+bingo[interacion1][interacion2]+'</td>'
-          }else{
-                printResultado +='<td>'+bingo[interacion1][interacion2]+'</td>'
-         }
-     }
-          printResultado += '</tr>'
+for (interacion1 = 0; interacion1 < 5; interacion1++) {
+    printResultado += '<tr>'
+
+    for (interacion2 = 0; interacion2 < 5; interacion2++) {
+   
+
+
+        if (interacion2 == 0) {
+            printResultado += '<td class="cambio1 celda-numeros celda-numeros2 celda-numeros3">' + bingo[interacion1][interacion2] + '</td>'
+        } else if (interacion2 == 1) {
+            printResultado += '<td class="cambio2 celda-numeros celda-numeros2 celda-numeros3">' + bingo[interacion1][interacion2] + '</td>'
+        } else if (interacion2 == 2) {
+            printResultado += '<td class="cambio3 celda-numeros celda-numeros2 celda-numeros3">' + bingo[interacion1][interacion2] + '</td>'
+        } else if (interacion2 == 3) {
+            printResultado += '<td class="cambio4 celda-numeros celda-numeros2 celda-numeros3">' + bingo[interacion1][interacion2] + '</td>'
+        } else {
+            printResultado += '<td class="cambio5 celda-numeros celda-numeros2 celda-numeros3">' + bingo[interacion1][interacion2] + '</td>'
+        }
+    }
+    printResultado += '</tr>'
 }
 printResultado += ' </tbody>'
 printResultado += '</table>'
-                                                              
 
-document.getElementById("bingo").innerHTML=printResultado
+
+document.getElementById("bingo").innerHTML = printResultado
 
 // boton de fila 1
- let button1 = document.getElementById('columna1')
- button1.addEventListener("click",()=>{
-    let cambio1 = document.querySelectorAll(".cambio1")
-    cambio1.forEach((Element)=>{
-        Element.style.color= "white"
+let button1 = document.getElementById('columna1')
+button1.addEventListener("click", () => { //funcion anonima
+    let cambio1 = document.querySelectorAll(".cambio1")///traigo todos los td que considian con la clase
+    cambio1.forEach((Element) => {
+        Element.style.color = "white"
         Element.style.background = "blue"
-        Element.style.transition = "all 2s ease" 
+        Element.style.transition = "all 2s ease"
     })
     let cambio2 = document.querySelectorAll(".cambio2")
-    cambio2.forEach((Element)=>{
-        Element.style.color= "#000"
+    cambio2.forEach((Element) => {
+        Element.style.color = "#000"
         Element.style.background = "white"
-        Element.style.transition = "all 2s ease" 
+        Element.style.transition = "all 2s ease"
     })
     let cambio3 = document.querySelectorAll(".cambio3")
-    cambio3.forEach((Element)=>{
-        Element.style.color= "#000"
+    cambio3.forEach((Element) => {
+        Element.style.color = "#000"
         Element.style.background = "white"
         Element.style.transition = "all 2s ease"
     })
     let cambio4 = document.querySelectorAll(".cambio4")
-    cambio4.forEach((Element)=>{
-        Element.style.color= "#000"
+    cambio4.forEach((Element) => {
+        Element.style.color = "#000"
         Element.style.background = "white"
-        Element.style.transition = "all 2s ease" 
+        Element.style.transition = "all 2s ease"
     })
     let cambio5 = document.querySelectorAll(".cambio5")
-    cambio5.forEach((Element)=>{
-        Element.style.color= "#000"
+    cambio5.forEach((Element) => {
+        Element.style.color = "#000"
         Element.style.background = "white"
-        Element.style.transition = "all 2s ease" 
+        Element.style.transition = "all 2s ease"
     })
- })
+})
 
 // boton de fila 2
- let button2 = document.getElementById('columna2')
- button2.addEventListener("click",()=>{
+let button2 = document.getElementById('columna2')
+button2.addEventListener("click", () => {
     let cambio2 = document.querySelectorAll(".cambio2")
-    cambio2.forEach((Element)=>{
-        Element.style.color= "white"
+    cambio2.forEach((Element) => {
+        Element.style.color = "white"
         Element.style.background = "#41b"
         Element.style.transition = "all 2s ease"
     })
     let cambio1 = document.querySelectorAll(".cambio1")
-    cambio1.forEach((Element)=>{
-        Element.style.color= "#000"
+    cambio1.forEach((Element) => {
+        Element.style.color = "#000"
         Element.style.background = "white"
-        Element.style.transition = "all 2s ease" 
+        Element.style.transition = "all 2s ease"
     })
     let cambio3 = document.querySelectorAll(".cambio3")
-    cambio3.forEach((Element)=>{
-        Element.style.color= "#000"
+    cambio3.forEach((Element) => {
+        Element.style.color = "#000"
         Element.style.background = "white"
         Element.style.transition = "all 2s ease"
     })
     let cambio4 = document.querySelectorAll(".cambio4")
-    cambio4.forEach((Element)=>{
-        Element.style.color= "#000"
+    cambio4.forEach((Element) => {
+        Element.style.color = "#000"
         Element.style.background = "white"
-        Element.style.transition = "all 2s ease" 
+        Element.style.transition = "all 2s ease"
     })
     let cambio5 = document.querySelectorAll(".cambio5")
-    cambio5.forEach((Element)=>{
-        Element.style.color= "#000"
+    cambio5.forEach((Element) => {
+        Element.style.color = "#000"
         Element.style.background = "white"
-        Element.style.transition = "all 2s ease" 
+        Element.style.transition = "all 2s ease"
     })
- })
+})
 
 // boton de fila 3
- let button3 = document.getElementById('columna3')
- button3.addEventListener("click",()=>{
+let button3 = document.getElementById('columna3')
+button3.addEventListener("click", () => {
     let cambio3 = document.querySelectorAll(".cambio3")
-    cambio3.forEach((Element)=>{
-        Element.style.color= "white"
+    cambio3.forEach((Element) => {
+        Element.style.color = "white"
         Element.style.background = "blue"
         Element.style.transition = "all 2s ease"
     })
     let cambio1 = document.querySelectorAll(".cambio1")
-    cambio1.forEach((Element)=>{
-        Element.style.color= "#000"
+    cambio1.forEach((Element) => {
+        Element.style.color = "#000"
         Element.style.background = "white"
-        Element.style.transition = "all 2s ease" 
+        Element.style.transition = "all 2s ease"
     })
     let cambio2 = document.querySelectorAll(".cambio2")
-    cambio2.forEach((Element)=>{
-        Element.style.color= "#000"
+    cambio2.forEach((Element) => {
+        Element.style.color = "#000"
         Element.style.background = "white"
-        Element.style.transition = "all 2s ease" 
+        Element.style.transition = "all 2s ease"
     })
     let cambio4 = document.querySelectorAll(".cambio4")
-    cambio4.forEach((Element)=>{
-        Element.style.color= "#000"
+    cambio4.forEach((Element) => {
+        Element.style.color = "#000"
         Element.style.background = "white"
-        Element.style.transition = "all 2s ease" 
+        Element.style.transition = "all 2s ease"
     })
     let cambio5 = document.querySelectorAll(".cambio5")
-    cambio5.forEach((Element)=>{
-        Element.style.color= "#000"
+    cambio5.forEach((Element) => {
+        Element.style.color = "#000"
         Element.style.background = "white"
-        Element.style.transition = "all 2s ease" 
+        Element.style.transition = "all 2s ease"
     })
- })
+})
 
 // boton de fila 4
- let button4 = document.getElementById('columna4')
- button4.addEventListener("click",()=>{
+let button4 = document.getElementById('columna4')
+button4.addEventListener("click", () => {
     let cambio4 = document.querySelectorAll(".cambio4")
-    cambio4.forEach((Element)=>{
-        Element.style.color= "white"
+    cambio4.forEach((Element) => {
+        Element.style.color = "white"
         Element.style.background = "#41b"
         Element.style.transition = "all 2s ease"
     })
     let cambio1 = document.querySelectorAll(".cambio1")
-    cambio1.forEach((Element)=>{
-        Element.style.color= "#000"
+    cambio1.forEach((Element) => {
+        Element.style.color = "#000"
         Element.style.background = "white"
-        Element.style.transition = "all 2s ease" 
+        Element.style.transition = "all 2s ease"
     })
     let cambio2 = document.querySelectorAll(".cambio2")
-    cambio2.forEach((Element)=>{
-        Element.style.color= "#000"
+    cambio2.forEach((Element) => {
+        Element.style.color = "#000"
         Element.style.background = "white"
-        Element.style.transition = "all 2s ease" 
+        Element.style.transition = "all 2s ease"
     })
     let cambio3 = document.querySelectorAll(".cambio3")
-    cambio3.forEach((Element)=>{
-        Element.style.color= "#000"
+    cambio3.forEach((Element) => {
+        Element.style.color = "#000"
         Element.style.background = "white"
-        Element.style.transition = "all 2s ease" 
+        Element.style.transition = "all 2s ease"
     })
     let cambio5 = document.querySelectorAll(".cambio5")
-    cambio5.forEach((Element)=>{
-        Element.style.color= "#000"
+    cambio5.forEach((Element) => {
+        Element.style.color = "#000"
         Element.style.background = "white"
-        Element.style.transition = "all 2s ease" 
+        Element.style.transition = "all 2s ease"
     })
- })
+})
 
 // boton de fila 5
- let button5 = document.getElementById('columna5')
- button5.addEventListener("click",()=>{
+let button5 = document.getElementById('columna5')
+button5.addEventListener("click", () => {
     let cambio5 = document.querySelectorAll(".cambio5")
-    cambio5.forEach((Element)=>{
-        Element.style.color= "white"
+    cambio5.forEach((Element) => {
+        Element.style.color = "white"
         Element.style.background = "#41b"
         Element.style.transition = "all 2s ease"
     })
     let cambio1 = document.querySelectorAll(".cambio1")
-    cambio1.forEach((Element)=>{
-        Element.style.color= "#000"
+    cambio1.forEach((Element) => {
+        Element.style.color = "#000"
         Element.style.background = "white"
-        Element.style.transition = "all 2s ease" 
-    })
-    let cambio2 = document.querySelectorAll(".cambio2")
-    cambio2.forEach((Element)=>{
-        Element.style.color= "#000"
-        Element.style.background = "white"
-        Element.style.transition = "all 2s ease" 
-    })
-    let cambio3 = document.querySelectorAll(".cambio3")
-    cambio3.forEach((Element)=>{
-        Element.style.color= "#000"
-        Element.style.background = "white"
-        Element.style.transition = "all 2s ease" 
-    })
-    let cambio4 = document.querySelectorAll(".cambio4")
-    cambio4.forEach((Element)=>{
-        Element.style.color= "#000"
-        Element.style.background = "white"
-        Element.style.transition = "all 2s ease" 
-    })
- })
-
- // boton de una X
- let buttonX1 = document.getElementById('X1')
- buttonX1.addEventListener("click",()=>{
-    let cambioX1 = document.querySelectorAll(".cambioX")
-    cambioX1.forEach((Element)=>{
-        Element.style.color= "white"
-        Element.style.background = "#41b"
         Element.style.transition = "all 2s ease"
     })
- })
+    let cambio2 = document.querySelectorAll(".cambio2")
+    cambio2.forEach((Element) => {
+        Element.style.color = "#000"
+        Element.style.background = "white"
+        Element.style.transition = "all 2s ease"
+    })
+    let cambio3 = document.querySelectorAll(".cambio3")
+    cambio3.forEach((Element) => {
+        Element.style.color = "#000"
+        Element.style.background = "white"
+        Element.style.transition = "all 2s ease"
+    })
+    let cambio4 = document.querySelectorAll(".cambio4")
+    cambio4.forEach((Element) => {
+        Element.style.color = "#000"
+        Element.style.background = "white"
+        Element.style.transition = "all 2s ease"
+    })
+})
+
+let xGrande =[];
+let xPequeña =[];
+let x2Pequeña =[];
+
+
+//Grande
+for(let iteracion1 = 0; iteracion1 < 5; iteracion1++) {
+  for(let iteracion2 = 0; iteracion2 < 5; iteracion2++) {
+      if(iteracion1 == iteracion2 || iteracion1 + iteracion2 == 4) {
+          xGrande.push(bingo[iteracion1][iteracion2]);
+      }
+  }
+}
+let btnxGrande = document.getElementById("xGrande");
+let celdaNumeros = document.querySelectorAll(".celda-numeros");
+
+btnxGrande.addEventListener("click", () =>{
+
+    xGrande.forEach(number => {
+        celdaNumeros.forEach(element =>{
+        
+            let bingoNumber = element.textContent;
+         
+            if(bingoNumber == number){
+                element.style.color = "#000"
+                element.style.background = "red"
+                element.style.transition = "all 2s ease"
+            }
+        })
+    })
+})
+
+//xPEQUEÑA
+for (let iteracion1 = 2; iteracion1 < 5; iteracion1++) {
+  for (let iteracion2 = 0; iteracion2 < 3; iteracion2++) {
+    if (iteracion1%2==0&&iteracion2%2==0){
+      xPequeña.push(bingo[iteracion1][iteracion2]);
+    }else if (iteracion1%2==1&&iteracion2%2==1){
+      xPequeña.push(bingo[iteracion1][iteracion2]);
+    }else{
+      xPequeña.push("")
+    }
+  }
+}
+  let btnxPequeña = document.getElementById("xPequeña");
+  let celdaNumeros2 = document.querySelectorAll(".celda-numeros2");
+  
+  btnxPequeña.addEventListener("click", () =>{
+  
+      xPequeña.forEach(number => {
+          celdaNumeros2.forEach(element =>{
+          
+              let bingoNumber = element.textContent;
+           
+              if(bingoNumber == number){
+                  element.style.color = "#000"
+                  element.style.background = "green"
+                  element.style.transition = "all 2s ease"
+              }
+          })
+      })
+  })
+  
+//x2Pequeña
+  for (let iteracion1 = 2; iteracion1 < 5; iteracion1++) {
+    for (let iteracion2 = 2; iteracion2 < 5; iteracion2++) {
+      if (iteracion1%2==0&&iteracion2%2==0){
+        x2Pequeña.push(bingo[iteracion1][iteracion2]);
+      }else if (iteracion1%2==1&&iteracion2%2==1){
+        x2Pequeña.push(bingo[iteracion1][iteracion2]);
+      }else{
+        x2Pequeña.push("")
+      }
+    }
+  }
+    let btnx2Pequeña = document.getElementById("x2Pequeña");
+    let celdaNumeros3 = document.querySelectorAll(".celda-numeros3");
+    
+    btnx2Pequeña.addEventListener("click", () =>{
+    
+        x2Pequeña.forEach(number => {
+            celdaNumeros3.forEach(element =>{
+            
+                let bingoNumber = element.textContent;
+             
+                if(bingoNumber == number){
+                    element.style.color = "#000"
+                    element.style.background = "pink"
+                    element.style.transition = "all 2s ease"
+                }
+            })
+        })
+    })
+    
+  
